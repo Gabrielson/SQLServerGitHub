8b86f5df6b0521c964d711441c74850dab5ba888
Gabrielson <gabrielson@roanoke.edu>
2018-09-24 09:24:28 -0400

    removed test table

--- Tables/dbo.test_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[test_table]
-(
-[test] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
